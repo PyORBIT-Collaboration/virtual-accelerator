8989a51b71bb6b8d2d44dde49293ae79e81e4c20
4wc <catheybl@ornl.gov>
2023-09-28 12:02:08 -0400

    trackSCL2 scans through multiple cavities. BPM_plot looks at the data created by trackSCL2.

--- SCL Wizard/BPM_plot.py
+++ SCL Wizard/BPM_plot.py
@@ -1,11 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-current_wave = np.load("current.npy")
-beta_wave = np.load("beta.npy")
+beta_wave = np.load("delta_energies.npy")
 
-num_phases = current_wave.shape[1]
+num_phases = beta_wave.shape[0]
 
 for i in range(num_phases):
-    plt.plot(beta_wave[:, i])
+    plt.plot(beta_wave[i,:])
 plt.show()
--- SCL Wizard/trackSCL2.py
+++ SCL Wizard/trackSCL2.py
@@ -1,4 +1,3 @@
-
 #! /usr/bin/env python
 
 """
@@ -52,9 +51,11 @@ from orbit.py_linac.overlapping_fields import SNS_EngeFunctionFactory
 
 random.seed(100)
 
-phase_num = 25
+phase_num = 51
 
-tank = "SCL:Cav01a"
+#tank = "SCL:Cav01a"
+
+BPMs = ["SCL_Diag:BPM31", "SCL_Diag:BPM32"]
 
 bunch_file = "SCL_in.dat"
 
@@ -77,24 +78,44 @@ bunch_in = Bunch()
 bunch_in.readBunch(bunch_file)
 bunch_in.getSyncParticle().time(0.0)
 
+print(bunch_in.getSizeGlobal())
+for n in range(bunch_in.getSizeGlobal()):
+    if n+1 > 10000:
+        bunch_in.deleteParticleFast(n)
+bunch_in.compress()
+print(bunch_in.getSizeGlobal())
+
 print("Bunch Generation completed.")
-print(bunch_in.getSyncParticle().time(), bunch_in.getSyncParticle().beta())
+#print(bunch_in.getSyncParticle().time(), bunch_in.getSyncParticle().beta())
 
 
-BPMs = accLattice_SCL.getNodesForSubstring("BPM", "drift")
-current_waveform = np.full([phase_num, len(BPMs)], -1.0)
-beta_waveform = np.full([phase_num, len(BPMs)], -1.0)
+cavities = accLattice_SCL.getNodesForSubstring("Cav", "drift")
+cavDict = {}
+cav_count = 0
+for subcavity in cavities:
+    cavity = subcavity.getRF_Cavity()
+    if cavity.getName() not in cavDict:
+        cavity.setAmp(0.0)
+        cavDict[cavity.getName()] = cav_count
+        cav_count += 1
+        #if cavity.getName() == tank:
+        #    cavDict[cavity.getName()] = cavity
+        #    cavity.setAmp(1.0)
 
-class BPM():
+#current_waveform = np.full([phase_num, len(BPMs)], -1.0)
+phase_waveform = np.full([len(cavDict), phase_num, len(BPMs)], -1.0)
+waveformDict = {}
+
+class BPM_class():
     def trackActions(actionsContainer, paramsDict):
         bunch = paramsDict["bunch"]
         part_num = bunch.getSizeGlobal()
         if part_num > 0:
-            name = paramsDict["parentNode"].getName()
-            phase = paramsDict["lattice"].getNodeForName("SCL_RF:Cav01a:Rg01").getRF_Cavity().getPhase()
-            beta = bunch.getSyncParticle().beta()
-            phase_ind = np.where(phases == phase)[0][0]
-            BPM_ind = BPMsDict[name]
+            BPM_name = paramsDict["parentNode"].getName()
+            Synch_phase = (bunch.getSyncParticle().time() * 402.5e6 * 2 * np.pi) % (2 * np.pi) - np.pi
+            #cavphase = paramsDict["lattice"].getNodeForName("SCL_RF:Cav01a:Rg01").getRF_Cavity().getPhase()
+            #phase_ind = np.where(phases == phase)[0][0]
+            #BPM_ind = BPMsDict[BPM_name]
             """
             x_avg, xp_avg, y_avg, yp_avg, z_avg, dE_avg = 0, 0, 0, 0, 0, 0
             for n in range(part_num):
@@ -113,26 +134,17 @@ class BPM():
             dE_avg /= part_num
             #str = f"{name} {part_num} {beta} {x_avg} {xp_avg} {y_avg} {yp_avg} {z_avg} {dE_avg}\n"
             """
-            current_waveform[phase_ind,BPM_ind] = part_num
-            beta_waveform[phase_ind, BPM_ind] = beta
+            #current_waveform[phase_ind,BPM_ind] = part_num
+            #phase_waveform[phase_ind, BPM_ind] = measured_phase
+            waveformDict[BPM_name] = Synch_phase
 
 BPMsDict = {}
-bpm_count = 0
-for BPM_node in BPMs:
-    BPM_node.addChildNode(BPM, BPM_node.ENTRANCE)
-    BPMsDict[BPM_node.getName()] = bpm_count
-    bpm_count += 1
-
-
-cavities = accLattice_SCL.getNodesForSubstring("Cav", "drift")
-cavDict = {}
-for subcavity in cavities:
-    cavity = subcavity.getRF_Cavity()
-    if cavity.getName() not in cavDict:
-        cavity.setAmp(0.0)
-        if cavity.getName() == tank:
-            cavDict[cavity.getName()] = cavity
-            cavity.setAmp(1.0)
+BPM_count = 0
+for BPM in BPMs:
+    BPM_node = accLattice_SCL.getNodeForName(BPM)
+    BPM_node.addChildNode(BPM_class, BPM_node.ENTRANCE)
+    BPMsDict[BPM] = BPM_count
+    BPM_count += 1
 
 # track through the lattice
 paramsDict = {"old_pos": -1.0, "count": 0, "pos_step": 0.1}
@@ -142,8 +154,21 @@ for p in range(phase_num):
     phase = (p / (phase_num - 1) * 2 - 1) * np.pi
     phases[p] = phase
 
+#T_array = np.full([len(cavDict), phase_num], -1.0)
+DT = np.full([len(cavDict), phase_num], -1.0)
+limits = np.full([len(cavDict), 2], -1.0)
+energies = np.load("energies.npy")
+for cav in range(energies.shape[0]):
+    Pmax = np.max(energies[cav,:])
+    Pmin = np.min(energies[cav, :])
+    limits[cav,:] = Pmin, Pmax
+
+dist = accLattice_SCL.getNodeForName(BPMs[1]).getPosition() - accLattice_SCL.getNodeForName(BPMs[0]).getPosition()
+Terror = 1e-6
+
 for cav_name in cavDict:
-    cavity = cavDict[cav_name]
+    cavity = accLattice_SCL.getRF_Cavity(cav_name)
+    cavity.setAmp(1.0)
     for phase in phases:
         cavity.setPhase(phase)
 
@@ -153,11 +178,53 @@ for cav_name in cavDict:
         bunch_in.readBunch(bunch_file)
         bunch_in.getSyncParticle().time(0.0)
 
+        for n in range(bunch_in.getSizeGlobal()):
+            if n + 1 > 100:
+                bunch_in.deleteParticleFast(n)
+        bunch_in.compress()
+
         accLattice_SCL.trackDesignBunch(bunch_in)
         accLattice_SCL.trackBunch(bunch_in)
 
-np.save('current', current_waveform)
-np.save('beta', beta_waveform)
+        phase1, phase0 = waveformDict[BPMs[1]], waveformDict[BPMs[0]]
 
+        realT = bunch_in.getSyncParticle().kinEnergy()
+        #T_array[cavDict[cav_name], np.where(phases == phase)[0][0]] = T
+
+        T = 100000000
+        n = 0
+        count = 0
+        while np.abs(realT - T) > Terror:
+            t = ((phase1 + n * 2 * np.pi) - phase0) / (402.5e6 * 2 * np.pi)
+            beta = dist / t / 299792458
+            if beta < 1 and beta > 0:
+                gamma = 1 / math.sqrt(1 - beta * beta)
+                T = (gamma - 1) * 0.939294
+                #print(n, beta, gamma, T)
+            n += 1
+
+        DT[cavDict[cav_name], np.where(phases == phase)[0][0]] = realT - T
+
+#np.save('energies', T_array)
+np.save('delta_energies', DT)
+#np.save('energies_count', T_count)
+
+sys.exit()
+
+#np.save('current', current_waveform)
+np.save('phases', phase_waveform)
+
+d = accLattice_SCL.getNodeForName(BPMs[1]).getPosition() - accLattice_SCL.getNodeForName(BPMs[0]).getPosition()
+beta = 1
+n = 0
+while beta >= 0.3:
+    t = ((phase_waveform[0,1] + n * 2 * np.pi) - phase_waveform[0,0]) / (402.5e6 * 2 * np.pi)
+    beta = d / t / 299792458
+    if beta < 1:
+        gamma = 1 / math.sqrt(1 - beta * beta)
+        T = (gamma - 1) * 0.939294
+        print(n, beta, gamma, T)
+    n += 1
+print(n-1, beta, gamma, T)
 
 print("Tracking completed.")
