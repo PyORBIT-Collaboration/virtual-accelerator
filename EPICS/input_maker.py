0fd4690f94a6c5d9b490be9b423bc2992210666c
Zhukov, Alexander P <zhukovap@ornl.gov>
2023-12-18 18:56:00 -0500

    Generalizing VA scripts to work with MEBT

--- EPICS/input_maker.py
+++ EPICS/input_maker.py
@@ -1,58 +1,94 @@
 import json
 from pathlib import Path
 from random import random
+import argparse
 
 from orbit.py_linac.linac_parsers import SNS_LinacLatticeFactory
 
 from pyorbit_server_interface import OrbitModel
 
-config_file = Path("va_config.json")
-offset_file = Path("va_offsets.json")
 
-lattice_str = "sns_linac.xml"
-subsections = ['SCLMed', 'SCLHigh', 'HEBT1']
 
-sns_linac_factory = SNS_LinacLatticeFactory()
-sns_linac_factory.setMaxDriftLength(0.01)
-model_lattice = sns_linac_factory.getLinacAccLattice(subsections, lattice_str)
+def main():
+    mebt_map = {
+        "MEBT_RF:Bnch01": "MEBT_LLRF:FCM1",
+        "MEBT_RF:Bnch02": "MEBT_LLRF:FCM2",
+        "MEBT_RF:Bnch03": "MEBT_LLRF:FCM3",
+        "MEBT_RF:Bnch04": "MEBT_LLRF:FCM4",
 
-model = OrbitModel(model_lattice)
+    }
 
-devices = {'Cavities': {},
-           'Quadrupoles': {},
-           'Correctors': {},
-           'Wire_Scanners': {},
-           'BPMs': {},
-           'PBPMs': {}, }
+    description = f"""
+            Generates config files for accelerator sequences
+            """
+    epilog = """
+            Example: input_maker SCLMed SCLHigh
+            """
+    parser = argparse.ArgumentParser( description=description, epilog=epilog,
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--file', '-f', default='va_config.json', type=str,
+                        help='Pathname of resulting config json file.')
+    parser.add_argument("Sequences", nargs='*', help='Sequences', default=['SCLMed', 'SCLHigh', 'HEBT1'])
+    args = parser.parse_args()
 
-offsets = {}
+    config_file = Path(args.file)
+    config_name = config_file.name.split('.')[0]
+    offset_name = config_name[0:-7] + '_offsets.json' if config_name.endswith('_config') else config_name + '_offset.json'
+    offset_file = config_file.parent / offset_name
 
-for or_name, ele_ref in model.pyorbit_dict.get_element_dictionary().items():
-    if 'Cav' in or_name:
-        pv_name = "SCL_LLRF:FCM" + or_name[-3:]
-        devices['Cavities'][pv_name] = or_name
-        offsets[pv_name] = (2 * random() - 1) * 180
-    elif 'Q' in or_name:
-        split_name = or_name.split(':')
-        pv_name = f"{split_name[0]}:PS_{split_name[1]}"
-        devices['Quadrupoles'][pv_name] = or_name
-    elif 'DC' in or_name:
-        split_name = or_name.split(':')
-        pv_name = f"{split_name[0]}:PS_{split_name[1]}"
-        devices['Correctors'][pv_name] = or_name
-    elif 'WS' in or_name:
-        pv_name = or_name
-        devices['Wire_Scanners'][pv_name] = or_name
-    elif 'BPM' in or_name:
-        pv_name = or_name
-        devices['BPMs'][pv_name] = or_name
-        offsets[pv_name] = (2 * random() - 1) * 180
+    lattice_str = "sns_linac.xml"
+    subsections = args.Sequences
 
-        pv_name = pv_name.replace('Diag', 'Phys')
-        devices['PBPMs'][pv_name] = or_name
 
-with open(config_file, "w") as json_file:
-    json.dump(devices, json_file, indent=4)
+    sns_linac_factory = SNS_LinacLatticeFactory()
+    sns_linac_factory.setMaxDriftLength(0.01)
+    model_lattice = sns_linac_factory.getLinacAccLattice(subsections, lattice_str)
 
-with open(offset_file, "w") as json_file:
-    json.dump(offsets, json_file, indent=4)
+    model = OrbitModel(model_lattice)
+
+    devices = {'Cavities': {},
+               'Quadrupoles': {},
+               'Correctors': {},
+               'Wire_Scanners': {},
+               'BPMs': {},
+               'PBPMs': {}, }
+
+    offsets = {}
+
+    for or_name, ele_ref in model.pyorbit_dict.get_element_dictionary().items():
+        if or_name in mebt_map:
+            pv_name = mebt_map[or_name]
+            devices['Cavities'][pv_name] = or_name
+            offsets[pv_name] = (2 * random() - 1) * 180
+        elif 'Cav' in or_name:
+            pv_name = "SCL_LLRF:FCM" + or_name[-3:]
+            devices['Cavities'][pv_name] = or_name
+            offsets[pv_name] = (2 * random() - 1) * 180
+        elif 'Q' in or_name:
+            split_name = or_name.split(':')
+            pv_name = f"{split_name[0]}:PS_{split_name[1]}"
+            devices['Quadrupoles'][pv_name] = or_name
+        elif 'DC' in or_name:
+            split_name = or_name.split(':')
+            pv_name = f"{split_name[0]}:PS_{split_name[1]}"
+            devices['Correctors'][pv_name] = or_name
+        elif 'WS' in or_name:
+            pv_name = or_name
+            devices['Wire_Scanners'][pv_name] = or_name
+        elif 'BPM' in or_name:
+            pv_name = or_name
+            devices['BPMs'][pv_name] = or_name
+            offsets[pv_name] = (2 * random() - 1) * 180
+
+            pv_name = pv_name.replace('Diag', 'Phys')
+            devices['PBPMs'][pv_name] = or_name
+
+    with open(config_file, "w") as json_file:
+        json.dump(devices, json_file, indent=4)
+
+    with open(offset_file, "w") as json_file:
+        json.dump(offsets, json_file, indent=4)
+
+if __name__ == "__main__":
+    main()
+
--- EPICS/virtual_accelerator.py
+++ EPICS/virtual_accelerator.py
@@ -3,6 +3,7 @@
 import json
 import time
 import argparse
+from pathlib import Path
 
 from orbit.core.bunch import Bunch
 from orbit.py_linac.lattice_modifications import Add_quad_apertures_to_lattice, Add_rfgap_apertures_to_lattice
@@ -21,14 +22,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run CA server')
     # parser.add_argument('--prefix', '-p', default='test', type=str, help='Prefix for PVs')
     parser.add_argument('--file', '-f', default='va_config.json', type=str,
-                        help='Pathname of pv file. Relative to Server/')
+                        help='Pathname of config json file.')
+    parser.add_argument('--bunch', default='bunch_in.dat', type=str,
+                        help='Pathname of input bunch file.')
+    parser.add_argument('--debug', dest='debug', action='store_true', help="Some debug info will be printed.")
+    parser.add_argument('--production', dest='debug', action='store_false',
+                        help="DEFAULT: No additional info printed.")
+
+    parser.add_argument("Sequences", nargs='*', help='Sequences', default=['SCLMed', 'SCLHigh', 'HEBT1'])
 
     args = parser.parse_args()
-    # prefix = args.prefix + ':'
-    # print(f'Using prefix: {args.prefix}.')
-
+    debug = args.debug
+    bunch_file = Path(args.bunch)
+    config_file = Path(args.file)
+    config_name = config_file.name.split('.')[0]
+    offset_name = config_name[0:-7] + '_offsets.json' if config_name.endswith('_config') else config_name + '_offset.json'
+    offset_file = config_file.parent / offset_name
     lattice_file = 'sns_linac.xml'
-    subsections = ['SCLMed', 'SCLHigh', 'HEBT1']
+    subsections = args.Sequences
+
     sns_linac_factory = SNS_LinacLatticeFactory()
     sns_linac_factory.setMaxDriftLength(0.01)
     model_lattice = sns_linac_factory.getLinacAccLattice(subsections, lattice_file)
@@ -36,7 +48,7 @@ if __name__ == '__main__':
     Add_rfgap_apertures_to_lattice(model_lattice)
 
     bunch_in = Bunch()
-    bunch_in.readBunch('SCL_in.dat')
+    bunch_in.readBunch(str(bunch_file))
     for n in range(bunch_in.getSizeGlobal()):
         if n + 1 > 1000:
             bunch_in.deleteParticleFast(n)
@@ -47,10 +59,10 @@ if __name__ == '__main__':
     # server = Server(prefix)
     server = Server()
 
-    with open(args.file, "r") as json_file:
+    with open(config_file, "r") as json_file:
         devices_dict = json.load(json_file)
 
-    with open('va_offsets.json', "r") as json_file:
+    with open(offset_file, "r") as json_file:
         offset_dict = json.load(json_file)
 
     for device_type, devices in devices_dict.items():
@@ -89,6 +101,8 @@ if __name__ == '__main__':
                 pbpm_device = PBPM(name, model_name)
                 server.add_device(pbpm_device)
 
+    if debug:
+        print(server)
     server.start()
     print(f"Server started.")
 
