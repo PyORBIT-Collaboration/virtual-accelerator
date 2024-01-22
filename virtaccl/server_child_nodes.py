438a47a1cd21a97448825b18cd67dbb5aae45bd7
4wc <catheybl@ornl.gov>
2024-01-22 10:52:04 -0500

    Changed the amplitudeAvg PV from giving the current to reporting the proper exponential.

--- virtaccl/server_child_nodes.py
+++ virtaccl/server_child_nodes.py
@@ -12,8 +12,8 @@ from orbit.core.bunch import Bunch
 # that can't be directly measured (like energy).
 class BPMclass:
     def __init__(self, child_name: str, frequency: float = 402.5e6):
-        self.parameters = {'frequency': frequency, 'x_avg': 0.0, 'y_avg': 0.0, 'phi_avg': 0.0, 'current': 0.0,
-                           'energy': 0.0, 'beta': 0.0}
+        self.parameters = {'frequency': frequency, 'x_avg': 0.0, 'y_avg': 0.0, 'phi_avg': 0.0, 'amp_avg': 0.0,
+                           'current': 0.0, 'energy': 0.0, 'beta': 0.0}
         self.child_name = child_name
         self.node_type = 'BPM'
         self.si_e_charge = 1.6021773e-19
@@ -30,27 +30,32 @@ class BPMclass:
             phase_coeff = 2 * math.pi / (sync_beta * 2.99792458e8 / rf_freq)
             sync_phase = (sync_part.time() * rf_freq * 2 * math.pi) % (2 * math.pi) - math.pi
             sync_energy = sync_part.kinEnergy()
-            x_avg, y_avg, z_avg = 0, 0, 0
+            x_avg, y_avg, z_avg, z_rms = 0, 0, 0, 0
             for n in range(part_num):
                 x, y, z = bunch.x(n), bunch.y(n), bunch.z(n)
                 x_avg += x
                 y_avg += y
                 z_avg += z
+                z_rms += z * z
             x_avg /= part_num
             y_avg /= part_num
             z_avg /= part_num
+            phi_rms = phase_coeff * math.sqrt(z_rms / part_num)
             phi_avg = phase_coeff * z_avg + sync_phase
+            amp = current * math.exp(-phi_rms * phi_rms / 2)
             self.parameters['x_avg'] = x_avg
             self.parameters['y_avg'] = y_avg
             self.parameters['phi_avg'] = phi_avg
+            self.parameters['amp_avg'] = amp
             self.parameters['current'] = current
             self.parameters['energy'] = sync_energy
             self.parameters['beta'] = sync_beta
-            # print(BPM_name + " : " + str(x_avg))
+            # print(BPM_name + " : " + str(amp))
         else:
             self.parameters['x_avg'] = 0.0
             self.parameters['y_avg'] = 0.0
             self.parameters['phi_avg'] = 0.0
+            self.parameters['amp_avg'] = 0.0
             self.parameters['current'] = 0.0
             self.parameters['energy'] = 0.0
             self.parameters['beta'] = 0.0
