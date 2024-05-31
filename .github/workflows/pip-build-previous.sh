2d66cec9fab7fb793e1b71681c35eebfd45ddd84
4wc <catheybl@ornl.gov>
2024-05-31 12:50:58 -0400

    Typo in pip-build-previous.sh.

--- .github/workflows/pip-build-previous.sh
+++ .github/workflows/pip-build-previous.sh
@@ -3,5 +3,5 @@ source .po3_va_previous/bin/activate
 pip install -U pip
 pip install git+https://github.com/PyORBIT-Collaboration/PyORBIT3
 commit_id=$(git log --format="%H" -n 1)
-pip install git+https://github.com/PyORBIT-Collaboration/virtual-accelerator.git@"$commit_id"
+pip install git+https://github.com/PyORBIT-Collaboration/virtual-accelerator.git@+"$commit_id"
 virtual_accelerator --print_pvs > old_pvs.txt
