9580e3b3e2686f5f918d02efb65b0f0a4aab28b0
4wc <catheybl@ornl.gov>
2024-05-31 09:49:44 -0400

    connection_test.py now connects to PVs more efficiently and prints it's results.

--- .github/workflows/connection_test.py
+++ .github/workflows/connection_test.py
@@ -1,8 +1,13 @@
-from epics import caget
+import sys
+import epics
 
 file1 = open('old_pvs.txt', 'r')
 Lines = file1.readlines()
 for line in Lines:
     line = line.split()
-    pv = line[0]
-    caget(pv)
+    pv_name = line[0]
+    pv = epics.PV(pv_name)
+    if pv.get() is None:
+        print("Some old PVs not found!")
+        sys.exit()
+print("All old PVs found!")
