593985443960db9715416f488b2542e0dbb4cdfc
4wc <catheybl@ornl.gov>
2024-05-31 09:56:50 -0400

    Moved to correct directory before generating list of PVs.

--- .github/workflows/pv-test.sh
+++ .github/workflows/pv-test.sh
@@ -1,7 +1,8 @@
+cd .github/workflows || exit
+
 . .po3_va_previous/bin/activate
 virtual_accelerator --print_pvs > old_pvs.txt
 
 . .po3_va/bin/activate
 virtual_accelerator &
-cd .github/workflows || exit
 python connection_test.py
