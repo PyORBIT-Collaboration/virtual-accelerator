eebb1002382bde69086a00dfb39e9cbf36c6e889
4wc <catheybl@ornl.gov>
2024-05-31 09:03:34 -0400

    Corrected pv-test to find cconnection_test.py. Also changed git revert in pip-build-previous.sh to not create a new commit.

--- .github/workflows/pip-build-previous.sh
+++ .github/workflows/pip-build-previous.sh
@@ -2,5 +2,5 @@ python3.9 -m venv .po3_va_previous
 source .po3_va_previous/bin/activate
 pip install -U pip
 pip install git+https://github.com/PyORBIT-Collaboration/PyORBIT3
-git revert HEAD
+git revert --no-commit HEAD
 pip install .
--- .github/workflows/pv-test.sh
+++ .github/workflows/pv-test.sh
@@ -3,4 +3,5 @@ virtual_accelerator --print_pvs > old_pvs.txt
 
 . .po3_va/bin/activate
 virtual_accelerator &
+cd .github/workflows || exit
 python connection_test.py
