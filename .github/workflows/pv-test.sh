42693fa62b003bb232bb0211183ca5ad514cebd7
4wc <catheybl@ornl.gov>
2024-05-31 10:07:22 -0400

    Typo in pv-test.sh.

--- .github/workflows/pv-test.sh
+++ .github/workflows/pv-test.sh
@@ -3,4 +3,4 @@ virtual_accelerator --print_pvs > old_pvs.txt
 
 . .po3_va/bin/activate
 virtual_accelerator &
-python .github/workflows/exitconnection_test.py
+python .github/workflows/connection_test.py
