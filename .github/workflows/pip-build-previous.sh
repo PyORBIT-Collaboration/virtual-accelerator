0ea47e459927c31ebafd061cd2ac66ed3bcb960b
4wc <catheybl@ornl.gov>
2024-06-03 13:06:55 -0400

    Continued attempts to install a previous version.

--- .github/workflows/pip-build-previous.sh
+++ .github/workflows/pip-build-previous.sh
@@ -3,6 +3,7 @@ source .po3_va_previous/bin/activate
 pip install -U pip
 pip install git+https://github.com/PyORBIT-Collaboration/PyORBIT3
 PREVIOUS_COMMIT=$(git rev-parse HEAD^1)
-git checkout "$PREVIOUS_COMMIT"
+echo "Previous commit SHA: $PREVIOUS_COMMIT"
+git checkout $PREVIOUS_COMMIT
 pip install .
 virtual_accelerator --print_pvs > old_pvs.txt
