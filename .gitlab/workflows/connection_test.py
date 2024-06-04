b645f9a28c44fd6ea444796270dc6260153147cd
4wc <catheybl@ornl.gov>
2024-06-04 10:03:51 -0400

    Created workflow for gitlab repository.

---
+++ .gitlab/workflows/connection_test.py
@@ -0,0 +1,12 @@
+import sys
+import epics
+
+file1 = open('old_pvs.txt', 'r')
+Lines = file1.readlines()
+for line in Lines:
+    line = line.split()
+    pv_name = line[0]
+    pv = epics.PV(pv_name)
+    if pv.get() is None:
+        raise ValueError("An error occurred: Some old PVs no longer connected.")
+print("All old PVs found!")
---
+++ .gitlab/workflows/corrector-test.sh
@@ -0,0 +1,2 @@
+. .po3_va/bin/activate
+python virtaccl/examples/Corrector.py
