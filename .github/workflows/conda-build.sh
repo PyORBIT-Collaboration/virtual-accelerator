5a3ad584bb865ccf3413ee4da48306c71bd33766
4wc <catheybl@ornl.gov>
2024-06-06 16:03:57 -0400

    Combined old and new builds into one conda-build.sh file as conda needs to update the same virac environment.

--- .github/workflows/conda-build.sh
+++ .github/workflows/conda-build.sh
@@ -1,4 +1,11 @@
 . /opt/conda/etc/profile.d/conda.sh
+PREVIOUS_COMMIT=$(git rev-parse HEAD^1)
+echo "Previous commit SHA: $PREVIOUS_COMMIT"
+git checkout $PREVIOUS_COMMIT
 conda env create -f virac.yml
+conda activate virac
+git checkout HEAD@{1}
+virtual_accelerator --print_pvs > old_pvs.txt
+conda env update --f virac.yml
 conda activate virac
 virtual_accelerator &
