2150b3efab9f520e7310136652e15c8f01fe8469
4wc <catheybl@ornl.gov>
2024-10-10 16:40:42 -0400

    Removed unneeded print from GitHub workflow to test for missing PVs.

--- .github/workflows/connection_test.py
+++ .github/workflows/connection_test.py
@@ -7,7 +7,6 @@ for line in Lines:
     line = line.split()
     pv_name = line[0]
     pv = epics.PV(pv_name)
-    print(pv_name)
     if pv.get() is None:
         raise ValueError("An error occurred: Some old PVs no longer connected.")
 print("All old PVs found!")
