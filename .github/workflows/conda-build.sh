3f0a3aed7781983a245a28bafe0bdd4c6f7c5a89
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-10-14 14:55:16 -0400

    Changed to mamba to allow caching. Removed unused scripts for automated testing.

--- .github/workflows/conda-build.sh
+++
@@ -1,5 +0,0 @@
-pwd
-# git config --global --add safe.directory /__w/virtual-accelerator/virtual-accelerator
-git status
-conda info
-pip list
--- .github/workflows/pv-test.sh
+++
@@ -1,3 +0,0 @@
-. /opt/conda/etc/profile.d/conda.sh
-conda activate virac
-python .github/workflows/connection_test.py
